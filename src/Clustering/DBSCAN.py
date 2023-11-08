c0b905df6bcc985a9fb15f87414eb54b839e1e5b
Ayush Agarwal <ayushagarwalds12@gmail.com>
2023-11-08 13:03:05 +0530

    DBSCAN done

---
+++ src/Clustering/DBSCAN.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+
+class DBSCAN:
+    def __init__(self, eps, min_samples):
+        self.eps = eps
+        self.min_samples = min_samples
+
+    def fit(self, X):
+        self.X = X
+        self.labels = [0] * len(X)
+        self.cluster_id = 0
+
+        for idx, point in enumerate(X):
+            if self.labels[idx] == 0:
+                if self.expand_cluster(point, idx):
+                    self.cluster_id += 1
+
+        return self.labels
+
+    def expand_cluster(self, point, idx):
+        seeds = self.region_query(point, idx)
+        if len(seeds) < self.min_samples:
+            self.labels[idx] = -1  # Mark as noise
+            return False
+        else:
+            self.cluster_id += 1
+            self.labels[idx] = self.cluster_id
+            for seed in seeds:
+                self.labels[seed] = self.cluster_id
+            while len(seeds) > 0:
+                current_point = seeds[0]
+                result = self.region_query(self.X[current_point], current_point)
+                if len(result) >= self.min_samples:
+                    for i in range(len(result)):
+                        result_point = result[i]
+                        if self.labels[result_point] == 0 or self.labels[result_point] == -1:
+                            if self.labels[result_point] == 0:
+                                seeds.append(result_point)
+                            self.labels[result_point] = self.cluster_id
+                seeds = seeds[1:]
+            return True
+
+    def region_query(self, point, idx):
+        seeds = []
+        for i in range(len(self.X)):
+            if np.linalg.norm(self.X[idx] - self.X[i]) < self.eps:
+                seeds.append(i)
+        return seeds
